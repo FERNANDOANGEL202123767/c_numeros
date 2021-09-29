dd383731845dc01c71104a300c148ffe8f824174
FERNANDOANGEL202123767 <fernandoangelhernandez62@gmail.com>
2021-09-28 21:50:09 -0500

    conjuntos numericos
    conjuntos numericos..
    axiomas y sus propiedades..

--- c_numeros.m
+++ c_numeros.m
@@ -10,39 +10,49 @@
 %                   :http://octaveintro.readthedocs.io/en/latest/index.html
 
 clear
-c_numeros_Naturales = '? = {1, 2, 3, .... n} si n > 0';
-c_numeros_Enteros= '?= { -n..., -2, -1, 0, 1, 2,..n }';
-c_numeros_Racionales='?= { m/n donde m,n ?? n ? 0}';
-c_numeros_Irracionales= 'I = {?n que no puede ser expresada como ? todas las raices que no son exactas'; 
-c_numeros_Reales= '?= { I, ? ?, ?}';
+c_numeros_Naturales = 'N = {1, 2, 3, .... n} si n > 0';
+c_numeros_Enteros= 'Z= { -n..., -2, -1, 0, 1, 2,..n }';
+c_numeros_Racionales='Q= { m/n donde m,n ?R n ? 0}';
+c_numeros_Irracionales= 'I = {?n que no puede ser expresada como Q todas las raices que no son exactas'; 
+c_numeros_Reales= '?= { I, Q Z, N}';
 
-% propiedades de los numeros,sean a,b,c,d,e ?? 
+% propiedades de los numeros,sean a,b,c,d,e ?R 
 
 % propiedades de ?(cerradura)
-p_cerradura = 'a + b ??';
-p_cerradura2 = 'ab ??';
-p_cerradura3 = '7 + 9 ??';
+p_cerradura = 'a + b ?R';
+p_cerradura2 = 'ab ?R';
+p_cerradura3 = '7 + 9 ?N';
 p_cerradura4 = '?= pertenencia';
 a=3;
 b=5;
+% Adicion
 a+b
+% Producto
 a*b
 
 % propiedad asociativa
 p_asociativa = 'a + (b + c )';
 p_asociativa2 = 'a (b c ) = ( a b ) c';
 p_asociativa3 = '3 + ( 8 - 10 ) = ( 8 + 3 ) - 10';
+a=3;
+b=5;
 c=10;
+% Adicion
 a+(b+c)
 (a+b)+c
+% Producto
+a*(b*c)
+(a*b)*c
 
 % propiedad conmutitativa
 p_conmutativa = 'a + b = b + a';
 p_conmutativa2 = 'a b = b a';
 a=3;
 b=5;
+% Adicion
 a+b
 b+a
+% Producto
 a*b
 b*a
 % propiedad distributiva
@@ -50,41 +60,40 @@ p_distributiva = 'a (b + c) = a b + a c';
 a=2;
 b=7;
 c=3;
+% Adicion
 a+(b+c)
+% Producto
 a*b+a*c
 
 %neutro aditivo
 p_neutroA = 'a + 0 = a';
 p_neutroA2 = 'Ojo: a + 0 = 0 + a ---> es conmutitativa';
-a=0;
-b=-1;
-c=1;
-a>b
-a+c>b+c
+a=4;
+% Adicion
+a+0
 
 % neutro multiplicativo
 p_neutroM = 'a ( 1 ) = a';
 a=2;
+% Producto
 a(1)
 
 % inverso aditivo
 p_inversoA = 'a + -a = 0';
 a=5;
+% Adicion inversa
 (-a)-5;
 a+(-a)
 
 % inverso multiplicativo o reciproco
 p_inversoM = 'a ( 1/a ) = 1';
 a=3;
-(a/1)
+% Producto inverso
+a*(1/a)
 
 % PROPIEDAD TRANSITIVA (| enconces)
 p_transitiva = 'si a > b y b > c | a > c';
 p_transitiva2 = 'si a = b y b = c | a= c';
-a=6;
-b=3;
-c=2;
-a>b>c | a>c
 
 % tricotomia (raiz del algebra) siempre se pueden comprar
 p_tricotomia = 'a > b';
